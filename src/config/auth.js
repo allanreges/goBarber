4cf13adefab41da4cd1b7da9eb0c4f4e05e1b04f
Allan de Carvalho <allan.reges@fbiz.com.br>
2020-04-01 00:34:46 -0300

    feature development

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'b993e4526238d62f6b1b90e605532ff8',
+  expiresIn: '7d',
+};
---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '963280e47b7265',
+    pass: 'a1d87f0d7971e6',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
